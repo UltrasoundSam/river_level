ba0c01bd7cea83098b0f358e06cf7809c25004cb
UltrasoundSam <samuel.hill@warwick.ac.uk>
2019-11-01 12:02:08 +0000

    Checks to see whether measurement already exists in database, and doesn't insert data if this is the case

--- main.py
+++ main.py
@@ -8,5 +8,12 @@ db = Database(Config)
 # Get new River level measurement
 dt, h, inFlood = river_level()
 
-sql_data_query = "INSERT INTO height (DateTime, level, in_Flood) VALUES('{0}', '{1}', '{2}')".format(dt.strftime('%Y-%m-%d %H:%M:%S'), h, inFlood)
-db.run_query(sql_data_query)
+# Check to see if measurement already exists
+dup_query = "SELECT DateTime from height WHERE DateTime = '{0}'".format(dt.strftime('%Y-%m-%d %H:%M:%S'))
+duplicates = db.run_query(dup_query)
+
+if not duplicates:
+    sql_data_query = "INSERT INTO height (DateTime, level, in_Flood) VALUES('{0}', '{1}', '{2}')".format(dt.strftime('%Y-%m-%d %H:%M:%S'), h, inFlood)
+    db.run_query(sql_data_query)
+else:
+    pass
