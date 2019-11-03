f453d2081a4339d3ef4835db8b90c5915742a172
UltrasoundSam <samuel.hill@warwick.ac.uk>
2019-11-03 11:19:23 +0000

    Changes layout as getting since measurement was unreliable (values weren't added every 15 minutes, but would be updated in clusters). Now, we check the database for the datetime of the last entry and get all the measurements since that time. Also allows us to run python script less often.

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from config import Config
 from database import Database
 from riverlevel import river_level
+import datetime
 
 # Initial database with config values
 db = Database(Config)
 
-# Get new River level measurement
-dt, h, inFlood = river_level()
+# Find when the last measurement in database was taken
+latest_query = "SELECT MAX(DateTime) from height"
+latest = db.run_query(latest_query)[0][0] + datetime.timedelta(minutes=5)
 
-# Check to see if measurement already exists
-dup_query = "SELECT DateTime from height WHERE DateTime = '{0}'".format(dt.strftime('%Y-%m-%d %H:%M:%S'))
-duplicates = db.run_query(dup_query)
+# Get new River level measurements (and reverse order so latest is first)
+Measures = river_level(since=latest)[::-1]
 
-if not duplicates:
+for h, dt, inFlood in Measures:
     sql_data_query = "INSERT INTO height (DateTime, level, in_Flood) VALUES('{0}', '{1}', '{2}')".format(dt.strftime('%Y-%m-%d %H:%M:%S'), h, inFlood)
     db.run_query(sql_data_query)
-else:
-    pass
+
--- riverlevel.py
+++ riverlevel.py
@@ -1,30 +1,40 @@
 import requests
 from datetime import datetime
 
-def river_level(station_id='L2406'):
+def river_level(since, station_id='L2406'):
     '''
     Function that calls the Environment Agency's API to get instanteous
     measurement of river levels at its numerous recording stations.
     Input:
+        since       - Datetime object to record all measurements since
         station_id  - Unique Station ID (defaults to Viking Recorder)
     Output:
-        (dt, level) - Tuple (date of measurement, river level)
+        (dt, level, inflood) - List of Tuples (date of measurement, river level)
     '''
+    # Datetime format
+    datestr = "%Y-%m-%dT%H:%M:%S%z"
+
+    # Find out Flood level
+    maxurl = 'https://environment.data.gov.uk/flood-monitoring/id/stations/{0}'.format(station_id)
+    flood = requests.get(maxurl).json()
+    FloodLevel = flood['items']['stageScale']['typicalRangeHigh']
+
     # Format URL and get data
-    requrl = 'https://environment.data.gov.uk/flood-monitoring/id/stations/{0}'.format(station_id)
+    requrl = 'https://environment.data.gov.uk/flood-monitoring/id/stations/{0}/readings?_sorted&since={1}'.format(station_id, since.strftime(datestr))
     payload = requests.get(requrl).json()
 
-    # Collect data
-    data = payload['items']['measures'][1]['latestReading']
-    Curlevel = data['value']
-    FloodLevel = payload['items']['stageScale']['typicalRangeHigh']
+    Measures = []
+    for item in payload['items']:
+        # Get current river level and time of measurement
+        Curlevel = item['value']
+        dt = datetime.strptime(item['dateTime'], datestr)
 
-    if Curlevel >= FloodLevel:
-        inFlood = 1
-    else:
-        inFlood = 0
+        # Check to see if river is currently in flood
+        if Curlevel >= FloodLevel:
+            inFlood = 1
+        else:
+            inFlood = 0
 
-    # Read timestamp
-    datestr = "%Y-%m-%dT%H:%M:%S%z"
-    dt = datetime.strptime(data['dateTime'], datestr)
-    return (dt, Curlevel, inFlood)
+        Measures.append((Curlevel, dt, inFlood))
+
+    return Measures
