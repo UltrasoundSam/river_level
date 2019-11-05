66b9863848d76d7fd4aeb1c2b0e7800760a7bdc4
UltrasoundSam <samuel.hill@warwick.ac.uk>
2019-11-05 19:14:02 +0000

    Checks to see if river level is higher or equal to that which means that the riverside path is flooded

--- main.py
+++ main.py
@@ -13,7 +13,16 @@ latest = db.run_query(latest_query)[0][0] + datetime.timedelta(minutes=5)
 # Get new River level measurements (and reverse order so latest is first)
 Measures = river_level(since=latest)[::-1]
 
+# Path is inpassable if river level > 3.6 m
+path_flood = 3.6
+
 for h, dt, inFlood in Measures:
-    sql_data_query = "INSERT INTO height (DateTime, level, in_Flood) VALUES('{0}', '{1}', '{2}')".format(dt.strftime('%Y-%m-%d %H:%M:%S'), h, inFlood)
-    db.run_query(sql_data_query)
+    if h >= path_flood:
+        path_closed = 1
+    else:
+        path_closed = 0
+
+    sql_data_query = "INSERT INTO height (DateTime, level, in_Flood, path_closed) VALUES('{0}', '{1}', '{2}', '{3}')"
+    
+    db.run_query(sql_data_query.format(dt.strftime('%Y-%m-%d %H:%M:%S'), h, inFlood, path_closed))
 
