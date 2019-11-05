c6b4e5592ec01ac3525c6054d9aa4d580b1fd8bf
UltrasoundSam <samuel.hill@warwick.ac.uk>
2019-11-05 19:56:29 +0000

    Sends an email using sendmail on Pi Server if river level goes above 3.6 m

--- main.py
+++ main.py
@@ -2,6 +2,22 @@ from config import Config
 from database import Database
 from riverlevel import river_level
 import datetime
+import os
+
+def warning_mail():
+    '''
+    Sends warning email to me when the path is flooded using mail
+    server on Pi
+    '''
+    # Get email to send alert to (in enviroment variables
+    to_addr = os.environ.get('EMAIL_TO')
+    
+    # Construct command-line message
+    cmd = 'echo "The path is currently flooded" | mail -s "Flood Alert" {0}'.format(to_addr)
+    
+    # Send it off to OS
+    os.popen(cmd)
+   
 
 # Initial database with config values
 db = Database(Config)
@@ -15,10 +31,13 @@ Measures = river_level(since=latest)[::-1]
 
 # Path is inpassable if river level > 3.6 m
 path_flood = 3.6
+send_mail = False
 
 for h, dt, inFlood in Measures:
     if h >= path_flood:
         path_closed = 1
+        # Dont want to send multiple emails if we have a large 
+        send_mail = True
     else:
         path_closed = 0
 
@@ -26,3 +45,5 @@ for h, dt, inFlood in Measures:
     
     db.run_query(sql_data_query.format(dt.strftime('%Y-%m-%d %H:%M:%S'), h, inFlood, path_closed))
 
+if send_mail:
+    warning_mail()
