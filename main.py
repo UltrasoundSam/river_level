310fd98494d56e2aba8820a376abc37c05f085a7
Sam Hill <sam.j.hill@protonmail.com>
2024-01-03 15:07:37 +0000

    Adds boilerplate to main

--- main.py
+++ main.py
@@ -26,48 +26,53 @@ def warning_mail(h: float, flood: bool) -> None:
     os.popen(cmd)
 
 
-# Initial database with config values
-db = Database(Config)
+def main():
+    # Initial database with config values
+    db = Database(Config)
 
-# Find when the last measurement in database was taken
-latest_query = "SELECT MAX(DateTime) from height"
-latest = db.run_query(latest_query)[0][0] + datetime.timedelta(minutes=5)
+    # Find when the last measurement in database was taken
+    latest_query = "SELECT MAX(DateTime) from height"
+    latest = db.run_query(latest_query)[0][0] + datetime.timedelta(minutes=5)
 
-# Check last 8 measurements to see if path was previously flooded
-path_query = "SELECT path_closed FROM height ORDER BY id DESC LIMIT 8"
-path_close = db.run_query(path_query)
-prev_flood = (1,) in path_close
+    # Check last 8 measurements to see if path was previously flooded
+    path_query = "SELECT path_closed FROM height ORDER BY id DESC LIMIT 8"
+    path_close = db.run_query(path_query)
+    prev_flood = (1,) in path_close
 
-# Get new River level measurements (and reverse order so latest is first)
-Measures = river_level(since=latest)[::-1]
+    # Get new River level measurements (and reverse order so latest is first)
+    Measures = river_level(since=latest)[::-1]
 
-# Path is inpassable if river level > 3.6 m
-path_flood = 3.6
-send_flood_mail = False
-send_drop_mail = False
+    # Path is inpassable if river level > 3.6 m
+    path_flood = 3.6
+    send_flood_mail = False
+    send_drop_mail = False
 
-for h, dt, inFlood in Measures:
-    if h >= path_flood:
-        path_closed = 1
-        flood_level = h
-        # Dont want to send multiple emails if we have a large
-        send_flood_mail = True
-    else:
-        path_closed = 0
-        # Check to see if path was previously flooded
-        if prev_flood:
-            send_drop_mail = True
+    for h, dt, inFlood in Measures:
+        if h >= path_flood:
+            path_closed = 1
             flood_level = h
+            # Dont want to send multiple emails if we have a large
+            send_flood_mail = True
+        else:
+            path_closed = 0
+            # Check to see if path was previously flooded
+            if prev_flood:
+                send_drop_mail = True
+                flood_level = h
 
-    sql_data_query = "INSERT INTO height (DateTime, level, in_Flood, path_closed) VALUES('{0}', '{1}', '{2}', '{3}')"
+        sql_data_query = "INSERT INTO height (DateTime, level, in_Flood, path_closed) VALUES('{0}', '{1}', '{2}', '{3}')"
 
-    db.run_query(sql_data_query.format(dt.strftime('%Y-%m-%d %H:%M:%S'), h, inFlood, path_closed))
+        db.run_query(sql_data_query.format(dt.strftime('%Y-%m-%d %H:%M:%S'), h, inFlood, path_closed))
 
-# Now we have dealt with backlog, and send emails if needed
-if send_flood_mail:
-    # River is in flood, but only send mail if it has only just flooded
-    if not prev_flood:
-        warning_mail(flood_level, flood=True)
-elif send_drop_mail:
-    # Send email out to say that the path is no longer flooded
-    warning_mail(flood_level, flood=False)
+    # Now we have dealt with backlog, and send emails if needed
+    if send_flood_mail:
+        # River is in flood, but only send mail if it has only just flooded
+        if not prev_flood:
+            warning_mail(flood_level, flood=True)
+    elif send_drop_mail:
+        # Send email out to say that the path is no longer flooded
+        warning_mail(flood_level, flood=False)
+
+
+if __name__ == '__main__':
+    main()
